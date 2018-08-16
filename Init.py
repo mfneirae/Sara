65d8a5670d212320cdd00086b322ad7a1b841c0f
Manuel Neira <mfneirae@unal.edu.co>
2018-08-16 12:12:29 -0500

    Selección

--- Init.py
+++ Init.py
@@ -16,21 +16,48 @@
 #       Or visit my webpage at https://mfneirae.com/
 # #############################################################################
 #
-#
+
+# IMPORT SECTION
+
 import time
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import os
+import getpass
+
+#INGRESAR USUARIO
+
+#username = input('Ingrese su usuario UNAL:             ')
+#password = getpass.getpass('Ingrese la contraseña de su correo:             ')
+
+#APERTURA DEL NAVEGADOR
 pwd = os.getcwd()
 cwd = 'browser = webdriver.Chrome(executable_path=r"'+ pwd +'\chromedriver.exe")'
 exec (cwd)
 browser.get('http://sara.unal.edu.co/saraweb/webcommon/acceso.jspx');
-NameLogin = browser.find_element_by_name('login:usuario')
-#NameLogin.send_keys('maguzmanp')
-NameLogin.send_keys('mfneirae')
-PassLogin = browser.find_element_by_name('login:password')
-PassLogin.send_keys('test123' + Keys.RETURN)
-time.sleep(5) # Let the user actually see something!
-browser.quit()
 
-DraSaChaPiPo2019
+#DATOS DE INGRESO
+
+NameLogin = browser.find_element_by_name('login:usuario')
+NameLogin.send_keys(username)
+PassLogin = browser.find_element_by_name('login:password')
+PassLogin.send_keys(password + Keys.RETURN)
+
+#INGRESO AL SARA
+
+time.sleep(1) # Let the user actually see something!
+thing = browser.find_element_by_id('popupMopdulo:j_id407:1:j_id411')
+thing.click()
+time.sleep(2)
+thing = browser.find_element_by_id('popupBusqueda_form:j_id425:0:j_id437')
+thing.click()
+time.sleep(2)
+thing = browser.find_element_by_id('formSalir:j_id43:j_id44:j_id652')
+Hover = ActionChains(browser).move_to_element(thing)
+link = browser.find_element_by_xpath("//span[@class='iceMnuBarItemLabel']").click()
+time.sleep(1)
+link = browser.find_element_by_xpath("//span[contains(text(),'Consultar Prog. Trab. Acad.')]").click()
+
+# time.sleep(5)
+# browser.quit()
